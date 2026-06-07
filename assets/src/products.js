104f5116c53cc47afda387ec64c6ae76375e8009
Ryan <257421077+ryanconceicao45-boop@users.noreply.github.com>
2026-06-06 21:33:50 -0300

    Versao 0.1

---
+++ assets/src/products.js
@@ -0,0 +1,18 @@
+const menuOptions = [
+    {
+        nome: 'X-Salada',
+        prince: 30,
+        vegan: false,
+        src: 'X-Salada',
+        ingredients: [
+            'Pão de hambúrguer',
+            'Hambúrguer bovino',
+            'Queijo mussarela',
+            'Alface',
+            'Tomate',
+            'Maionese',
+            'Ketchup',
+            'Mostarda',
+        ]
+    },
+]          
